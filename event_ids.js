2282c764b013f5b6dc1297312c922e9b45e0a1d2
Nathan Liu <nathan.liu@tablebook.me>
2018-11-14 20:30:28 +0000

    Initial commit

---
+++ event_ids.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+EVENT_IDS : {
+
+    '2018' : {
+        'monte' : {name: "rallye-automobile-de-monte-carlo", id: '42870'},
+        'sweden' : {name: "rally-sweden", id: '42875'},
+        'mexico' : {name: "rally-guanajuato-mexico", id: '44257'},
+        'corsica' : {name: "corsica-linea-tour-de-corse", id: '44258'},
+        'argentina' : {name: "ypf-rally-argentina", id: '44259'},
+        'portugal' : {name: "vodafone-rally-de-portugal", id: '44260'},
+        'sardinia' : {name: "rally-italia-sardegna", id: '44261'},
+        'finland' : {name: "neste-rally-finland", id: '44262'},
+        'germany' : {name: "adac-rallye-deutschland", id: '44263'},
+        'turkey' : {name: "rally-turkey-marmaris", id: '44264'},
+        'wales' : {name: "dayinsure-wales-rally-gb", id: '43469'},
+        'catalunya' : {name: "rallyracc-catalunya-costa-daurada", id: '44265'},
+        'australia' : {name: "kennards-hire-rally-australia", id: '44266'},
+    },
+
+    '2017' : {
+        'monte' : {name: "rallye-automobile-de-monte-carlo", id: '35143'},
+        'sweden' : {name: "rally-sweden", id: '35751'},
+        'mexico' : {name: "rally-guanajuato-mexico", id: '36014'},
+        'corsica' : {name: "che-guevara-energy-drink-tour-de-corse", id: '36015'},
+        'argentina' : {name: "ypf-rally-argentina", id: '36016'},
+        'portugal' : {name: "vodafone-rally-de-portugal", id: '36017'},
+        'sardinia' : {name: "rally-italia-sardegna", id: '36018'},
+        'poland' : {name: "orlen-rally-poland", id: '36431'},
+        'finland' : {name: "neste-rally-finland", id: '36019'},
+        'germany' : {name: "adac-rallye-deutschland", id: '36020'},
+        'catalunya' : {name: "rallyracc-catalunya-costa-daurada", id: '36021'},
+        'wales' : {name: "dayinsure-wales-rally-gb", id: '36022'},
+        'australia' : {name: "kennards-hire-rally-australia", id: '36023'},
+    },
+
+    '2016' : {
+        'monte' : {name: "rallye-automobile-de-monte-carlo", id: '25982'},
+        'sweden' : {name: "rally-sweden", id: '26241'},
+        'mexico' : {name: "rally-guanajuato-mexico", id: '27501'},
+        'argentina' : {name: "ypf-rally-argentina", id: '27493'},
+        'portugal' : {name: "vodafone-rally-de-portugal", id: '27503'},
+        'sardinia' : {name: "rally-italia-sardegna", id: '27500'},
+        'poland' : {name: "pzm-rajd-polski-rally-poland", id: '27502'},
+        'finland' : {name: "neste-rally-finland", id: '27496'},
+        'germany' : {name: "adac-rallye-deutschland", id: '27498'},
+        'corsica' : {name: "che-guevara-energy-drink-tour-de-corse", id: '27497'},
+        'catalunya' : {name: "rallyracc-catalunya-costa-daurada", id: '27504'},
+        'wales' : {name: "dayinsure-wales-rally-gb", id: '27499'},
+        'australia' : {name: "kennards-hire-rally-australia", id: '27494'},
+    }
+}
+
+}
---
+++ server.js
@@ -0,0 +1,177 @@
+"use strict"
+
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+const { EVENT_IDS } = require('./event_ids.js')
+
+app.get('/api/rally/getids/:year', function(req, res) {
+
+    const year = req.params.year;
+
+    res.send(EVENT_IDS[year])
+
+})
+
+app.get('/api/rally/:shortname/:year', function(req, res) {
+    //console.log(req.params)
+
+    const rallyYear = req.params.year;
+    const rallyName = EVENT_IDS[rallyYear][req.params.shortname].name; 
+    const rallyId = EVENT_IDS[rallyYear][req.params.shortname].id;
+
+    let url = `https://www.ewrc-results.com/results/${rallyId}-${rallyName}-${rallyYear}/`
+
+    console.log(url);
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            let $ = cheerio.load(html);
+
+            let driver, codriver, difference;
+            let json = { 
+                topTen : [
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                    { driver : "", codriver : "", difference: "" },
+                ]
+            }
+
+            for(var i = 1; i < 11;) {
+                
+
+                $(`.stage-results-after .table_sude:nth-child(${i}) a`).filter(function() {
+                    let data = $(this).text();
+                    let pair = data.split(' - ');
+                    driver = pair[0];
+                    codriver = pair[1];
+                })
+
+                $(`.stage-results-after .table_sude:nth-child(${i}) .stage-results-lost`).filter(function() {
+                    let data = $(this).text();
+                    let time = data.split('+')
+                    difference = '+' + time[1]
+                })
+
+                json.topTen[i-1].driver = driver;
+                json.topTen[i-1].codriver = codriver;
+                json.topTen[i-1].difference = difference;
+
+                i++;
+
+
+                $(`.stage-results-after .table_liche:nth-child(${i}) a`).filter(function() {
+                    let data = $(this).text();
+                    let pair = data.split(' - ');
+                    driver = pair[0];
+                    codriver = pair[1];
+                })
+
+                $(`.stage-results-after .table_liche:nth-child(${i}) .stage-results-lost`).filter(function() {
+                    let data = $(this).text();
+                    let time = data.split('+')
+                    difference = '+' + time[1]
+                })
+
+                json.topTen[i-1].driver = driver;
+                json.topTen[i-1].codriver = codriver;
+                json.topTen[i-1].difference = difference;
+
+                i++;
+            }
+
+            // Finally, we'll define the variables we're going to capture
+
+            res.send(json);
+        }
+    })
+
+
+})
+
+
+app.get('/api/championship/:year', function(req, res) {
+
+    let year = req.params.year; 
+    let url = `https://www.ewrc-results.com/season/${year}/1-wrc/`
+
+    console.log(url);
+
+    request(url, function(error, response, html){
+
+        if(!error){
+
+            let $ = cheerio.load(html);
+
+            let driver, pointsTotal;
+            let json = { 
+                topTen : [
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                    { driver : "", points : ""},
+                ]
+            }
+
+            for(var i = 2; i < 12;) {
+                        
+
+                $(`.table_sude:nth-child(${i}) a`).filter(function() {
+                    driver = $(this).text();
+                })
+
+                $(`.table_sude:nth-child(${i}) .points-total`).filter(function() {
+                    pointsTotal = $(this).text();
+                })
+
+                json.topTen[i-2].driver = driver;
+                json.topTen[i-2].points = pointsTotal;
+
+                i++;
+
+
+                $(`.table_liche:nth-child(${i}) a`).filter(function() {
+                    driver = $(this).text();
+                })
+
+                $(`.table_liche:nth-child(${i}) .points-total`).filter(function() {
+                    pointsTotal = $(this).text();
+                })
+
+                json.topTen[i-2].driver = driver;
+                json.topTen[i-2].points = pointsTotal;
+
+                i++;
+            }
+
+            res.send(json);
+        }
+    })
+})
+
+app.listen('8081')
+
+console.log('Running on port 8081')
+
+exports = module.exports = app;
